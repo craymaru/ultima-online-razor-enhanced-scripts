8aad263da46d2e90efa5442426d535fceec8d4bc
Craymaru <craymaru@gmail.com>
2020-11-23 17:42:49 +0900

    Added Utils Colors

---
+++ utils/colors.py
@@ -0,0 +1,73 @@
+# General
+normal = 0
+
+caution = 54
+warning = 54
+error = 32
+
+
+# Black
+black = 1380
+
+# White
+white = 1152
+
+# Gray
+gray = 0
+
+
+# Blue
+blue = 2
+
+bluepurple = 7
+
+sleepblue = 99
+
+# Purple
+purple = 12
+
+
+# Magenta
+magenta = 17
+
+vividpink = 22
+
+pinkred = 27
+sakura = 30
+
+
+# Red
+red = 32
+
+shu = 37
+redorange = 38
+
+
+# Orange
+orange = 42
+
+yamabuki = 47
+
+# Yellow
+yellow = 54
+
+
+# Green
+green = 62
+
+vividgreen = 57
+
+limegreen = 72
+
+# Cyan
+cyan = 84
+
+skyblue = 88
+mizu = 90
+
+
+
+# Gradetions
+milky = [1911, 1912, 1913, 1914, 1915, 1916, 1917, 1919, 1910]
+whitemilky = [1922, 1923, 1924, 1925, 1926, 1927, 1928, 1930, 1920]
+colorful = range(2501,2643)
