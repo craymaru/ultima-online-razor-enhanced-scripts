ac4ef8df6ff93ce8a1ab09ab82519cd117e38368
Craymaru <craymaru@gmail.com>
2020-11-23 20:59:48 +0900

    Added colors to utils

--- utils/colors.py
+++ utils/colors.py
@@ -1,73 +1,164 @@
-# General
+# Color Sample Image
+# https://hackmd.io/6U3Xv56QRlqpjfga2Gd8Cw?view
+
+
+# general
 normal = 0
 
-caution = 54
-warning = 54
-error = 32
+caution = 53
+warning = 38
+error = 33
 
 
-# Black
-black = 1380
-
-# White
+# mono
+gray = 0
+black = 1
 white = 1152
 
-# Gray
-gray = 0
+# 2 - 101
+
+# blue
+deepblue = 2
+blue = 3
+matblue = 4
+sleepblue = 5
+lightblue = 6
+
+# indigo
+deepindigo = 7
+indigo = 8
+matindigo = 9
+sleepindigo = 10
+lightindigo = 11
+
+# violet
+deepviolet = 12
+violet = 13
+matviolet = 14
+sleepviolet = 15
+lightviolet = 16
+
+# purple
+deeppurple = 17
+purple = 18
+matpurple = 19
+sleeppurple = 20
+lightpurple = 21
+
+# magenta
+deepmagenta = 22
+magenta = 23
+matmagenta = 24
+sleepmagenta = 25
+lightmagenta = 26
+
+# pink
+deeppink = 27
+pink = 28
+matpink = 29
+sleeppink = 30
+lightpink = 31
+
+# red
+deepred = 32
+red = 33
+matred = 34
+sleepred = 35
+lightred = 36
+
+# fire
+deepfire = 37
+fire = 38
+matfire = 39
+sleepfire = 40
+lightfire = 41
+
+# orange
+deeporange = 42
+orange = 43
+matorange = 44
+sleeporange = 45
+lightorange = 46
+
+# brown
+deepbrown = 47
+brown = 48
+matbrown = 49
+sleepbrown = 50
+lightbrown = 51
+
+# yellow
+deepyellow = 52
+yellow = 53
+matyellow = 54
+sleepyellow = 55
+lightyellow = 56
+
+# olive
+deepolive = 57
+olive = 58
+matolive = 59
+sleepolive = 60
+lightolive = 61
+
+# lime
+deeplime = 62
+lime = 63
+matlime = 64
+sleeplime = 65
+lightlime = 66
+
+# green
+deeplime = 67
+lime = 68
+matlime = 69
+sleeplime = 70
+lightlime = 71
+
+# clover
+deepclover = 72
+clover = 73
+matclover = 74
+sleepclover = 75
+lightclover = 76
+
+# emerald
+deepemerald = 77
+emerald = 78
+matemerald = 79
+sleepemerald = 80
+lightemerald = 81
+
+# mint
+deepmint = 82
+mint = 83
+matmint = 84
+sleepmint = 85
+lightmint = 86
+
+# cyan
+deepcyan = deepaqua = 87
+cyan = aqua = 88
+matcyan = mataqua = 89
+sleepcyan = sleepaqua = 90
+lightcyan = sleepaqua = 91
+
+# sky
+deepsky = 92
+sky = 93
+matsky = 94
+sleepsky = 95
+lightsky = 96
+
+# royal
+deeproyal = 97
+royal = 98
+matroyal = 99
+sleeproyal = 100
+lightroyal = 101
 
 
-# Blue
-blue = 2
-
-bluepurple = 7
-
-sleepblue = 99
-
-# Purple
-purple = 12
-
-
-# Magenta
-magenta = 17
-
-vividpink = 22
-
-pinkred = 27
-sakura = 30
-
-
-# Red
-red = 32
-
-shu = 37
-redorange = 38
-
-
-# Orange
-orange = 42
-
-yamabuki = 47
-
-# Yellow
-yellow = 54
-
-
-# Green
-green = 62
-
-vividgreen = 57
-
-limegreen = 72
-
-# Cyan
-cyan = 84
-
-skyblue = 88
-mizu = 90
-
-
-
-# Gradetions
-milky = [1911, 1912, 1913, 1914, 1915, 1916, 1917, 1919, 1910]
+# gradations
+milky = [1911, 1912, 1914, 1914, 1915, 1916, 1917, 1919, 1910]
 whitemilky = [1922, 1923, 1924, 1925, 1926, 1927, 1928, 1930, 1920]
 colorful = range(2501,2643)
